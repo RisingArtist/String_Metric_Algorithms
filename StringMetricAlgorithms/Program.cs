32633bb03410870702ded1c86e3713231d78e797
RisingArtist <guardianandre4@gmail.com>
2020-05-01 22:16:16 -0400

    Practice with Hamming, Levenshtein and Damerau-Levenshtein

--- StringMetricAlgorithms/Program.cs
+++ StringMetricAlgorithms/Program.cs
@@ -8,14 +8,15 @@ namespace StringMetricAlgorithms
 {
     class Program
     {
+        // Examples of Hamming, Levenshtein & Damerau-Levenshtein
+        // Number of operations would take to convert from String1 -> String2 or...
+        // ...Number of different characters between the two Strings
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.WriteLine("Compare 'Rem' and her sister 'Ram' names: " + StringMetric.Hamming("Rem", "Ram")); // 1
+            Console.WriteLine("Compare 'Luigi' and 'Waluigi' names: " + StringMetric.Levenshtein("Luigi", "Waluigi")); // 3
+            Console.WriteLine("Compare 'Fox McCloud' and 'James McCloud names: " + StringMetric.DamerauLevenshtein("Fox McCloud", "James McCloud")); // 5
+            Console.WriteLine("Compare 'Pikachu' and 'Pichu' names: " + StringMetric.Levenshtein("Pikachu", "Pichu")); // 2 
         }
     }
 }
---
+++ StringMetricAlgorithms/StringMetric.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMetricAlgorithms
+{
+    static class StringMetric
+    {
+        // Hamming Distance
+        public static int Hamming(string _fromWord, string _toWord)
+        {
+            if(_fromWord.Length != _toWord.Length)
+            {
+                Console.WriteLine("The Lengths are NOT the same!");
+                return -1;
+            }
+
+            int numOfDifferences = _fromWord.ToCharArray().Zip(_toWord.ToCharArray(), (c1, c2) => new { c1, c2 }).Count(m => m.c1 != m.c2 );
+            return numOfDifferences;
+        }
+
+        // Levenshtein Distance
+        public static int Levenshtein(string _fromWord, string _toWord, bool _showMatrix = false)
+        {
+            // Construct the Operation Matrix
+            int row = _fromWord.Length;
+            int column = _toWord.Length;
+            int[,] opMatrix = new int[row + 1, column + 1];
+
+
+            // Can't compare against a NULL string
+            if (row == 0 || column == 0)
+                return 0;
+
+            // Initialize the 1st row and column
+            for (int i = 0; i <= row; opMatrix[i, 0] = i++) { }
+            for (int i = 0; i <= column; opMatrix[0, i] = i++) { } 
+
+            // Iterate through the matrix
+            for(int i = 1; i <= row; i++)
+            {
+                for(int j = 1; j <= column; j++)
+                {
+                    // Are the current letters the same? 
+                    int cost = (_fromWord[i - 1] == _toWord[j - 1]) ? 0 : 1; 
+
+                    // Key - Number of Operations, where operations can be Insertion, Deletion or Replace
+                    // Note: The operations are NOT actually performed. 
+                    // |_A_|_B_|  =  | Delete  | Insert |
+                    // |_C_|_X_|     | Replace | Result |
+                    // 
+                    int deletion = opMatrix[i - 1, j] + 1;
+                    int insertion = opMatrix[i, j - 1] + 1;
+                    int substitution = opMatrix[i - 1, j - 1] + cost;
+                    opMatrix[i, j] = Math.Min(Math.Min(deletion, insertion) , substitution);
+                }
+            }
+
+            // Display Matrix, if toggled
+            if (_showMatrix)
+            {
+
+                for (int i = 0; i <= row; i++)
+                {
+                    for (int j = 0; j <= column; j++)
+                    {
+                        Console.Write("|" + opMatrix[i, j].ToString());
+                    }
+                    Console.WriteLine("");
+                }
+            }
+
+            // The total number of operations to convert fromWord -> ToWord is located...
+            return opMatrix[row, column]; // ...at the bottom right corner of the Operation Matrix
+        }
+
+        public static int DamerauLevenshtein(string _fromWord, string _toWord, bool _showMatrix = false)
+        {
+            // Construct the Operation Matrix
+            int row = _fromWord.Length; 
+            int column = _toWord.Length; 
+            int[,] opMatrix = new int[row + 1, column + 1];
+
+            // Can't compare against a NULL string
+            if (row == 0 || column == 0)
+                return 0;
+
+            // Initialize the 1st row and column
+            for (int i = 0; i < row; opMatrix[i, 0] = i++) { }
+            for (int j = 0; j < column; opMatrix[0, j] = j++) { };
+
+            // Iterate through the matrix
+            for (int i = 1; i < row; i++)
+            {
+                for (int j = 1; j < column; j++)
+                {
+                    int cost = (_fromWord[i - 1] == _toWord[j - 1]) ? 0 : 1;
+                    int insertion = opMatrix[i, j - 1] + 1;
+                    int deletion = opMatrix[i - 1, j] + 1;
+                    int substitution = opMatrix[i - 1, j - 1] + cost;
+
+                    int distance = Math.Min(insertion, Math.Min(deletion, substitution));
+
+                    // Transposition
+                    if (i > 1 && j > 1 && _fromWord[i - 1] == _toWord[j - 2] && _fromWord[i - 2] == _toWord[j - 1])
+                    {
+                        distance = Math.Min(distance, opMatrix[i - 2, j - 2] + cost);
+                    }
+
+                    opMatrix[i, j] = distance;
+                }
+            }
+
+            // Display Matrix, if toggled
+            if (_showMatrix)
+            {
+
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                    {
+                        Console.Write("|" + opMatrix[i, j].ToString());
+                    }
+                    Console.WriteLine("");
+                }
+            }
+
+            return opMatrix[row - 1, column - 1];
+        }
+    }
+}
